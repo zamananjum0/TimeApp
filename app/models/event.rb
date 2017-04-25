eb4b2ca7b306c341e0533176724c4ef63d2e67a4
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2017-04-25 18:28:18 +0500

    scheduler added

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,6 +20,19 @@ class Event < ApplicationRecord
         }
     }
   
+  def self.define_ranking
+    # events  = Event.where('end_date > ? AND end_date < ?', DateTime.now.beginning_of_day, DateTime.now).order('end_date DESC')
+    events  = Event.where('end_date < ?', DateTime.now).order('end_date DESC')
+    events && events.each do |event|
+      post =  Post.joins(:likes).select("posts.*, COUNT('likes.id') likes_count").where(likes: {likable_type: 'Post', is_like: true}, event_id: event.id).group('posts.id').order('likes_count DESC').try(:first)
+      if post.present?
+        event.post_id  = post.id
+        event.member_profile_id = post.member_profile_id
+        event.save!
+      end
+    end
+  end
+  
   def post_count
     self.posts.count
   end
